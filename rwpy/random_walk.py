7d2f9fd70ebc0418ad2bf71e7082a9d0498193aa
kaiser <kaisersunhk@gmail.com>
2019-11-20 14:46:13 -0800

    sum of two random walk

--- rwpy/random_walk.py
+++ rwpy/random_walk.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 def random_walk(trial_num, epoch):
-    distributions = np.random.randint(1, 3, size=(trial_num, epoch))
+    distributions = np.random.randint(1, 4, size=(trial_num, epoch))
     walks = np.where( distributions >= 2, 1, -1)
     walk_cumsums = np.cumsum(walks, axis=1)
     walk_cumsum_maxs = np.maximum.accumulate(walk_cumsums, axis=1)
@@ -11,5 +11,34 @@ def random_walk(trial_num, epoch):
     probability = np.count_nonzero(negative_walk_cumsum_max_counts > 0) / trial_num
     return probability
 
+
+def single_random_walk(epoch):
+    distributions = np.random.randint(1, 3, size=(1, epoch))
+    # distributions = np.random.randint(1, 3, size=(10, 10))
+    walks = np.where( distributions >= 2, 1, -1)
+    walk_cumsums = np.cumsum(walks, axis=1)
+    return walk_cumsums
+    
+def sum_random_walk(trial, X, epoch):
+    # Count the number of maximum appears at each index
+    count = np.full_like(X[0], 0)
+    # For loop, sum up to see where the maximum locates
+    for i in range(0, trial):
+        Y = single_random_walk(epoch)
+        twosum = X + Y
+        # Record the maximum in the array
+        max_value_indices = np.where(twosum == np.amax(twosum))
+        for index in max_value_indices:
+            count[index] += 1
+    return np.where(count == np.amax(count))
+
+        
+
+
 if __name__== "__main__":
-    print(random_walk(10000, 10000))
+    # Genderate a randomwalk X
+    X = single_random_walk(200)
+    # print(X)
+    # Sum up the random walk
+    print(sum_random_walk(200, X, 200))
+    # print(random_walk(10000, 10000))
